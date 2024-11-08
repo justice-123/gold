3296c21e52532cc7dcc1a8b995d20d5e49a4b429
Thomas Bale <tokbale@outlook.com>
2024-11-08 22:09:22 +0000

    k

--- Worker/worker.go
+++ Worker/worker.go
@@ -80,7 +80,7 @@ func main() {
 	flag.Parse()
 
 	rpc.Register(&Node{})
-	listener, err := net.Listen("tcp", "3.89.111.87:"+*serverPort)
+	listener, err := net.Listen("tcp", "172.31.86.145:"+*serverPort)
 	if err != nil {
 		log.Fatal("Listener error:", err)
 	}
