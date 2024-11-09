e6ed7992db0140dc005ca846b2d537b09bf45120
justice-123 <157595365+justice-123@users.noreply.github.com>
2024-11-09 02:54:57 +0000

    Update server.go

--- server/server.go
+++ server/server.go
@@ -315,7 +315,7 @@ func calculateNextWorld(currentWorld [][]uint8, size, workerNum int) [][]uint8 {
 		splitSegments[i] = make(chan [][]uint8)
 	}
 
-	serverAddress := "172.31.24.54"
+	serverAddress := "172.31.84.65"
 	workerPorts := [8]string{":8040", ":8050", ":8060", ":8070", ":8080", ":8090", ":9000", ":9010"}
 	workers := make([]*rpc.Client, workerNum)
 
@@ -407,7 +407,7 @@ func main() {
 	distWorkerNum = *workers
 
 	rpc.Register(&Server{})
-	listener, err := net.Listen("tcp", "54.173.43.196:"+*serverPort)
+	listener, err := net.Listen("tcp", "0.0.0.0:"+*serverPort)
 	if err != nil {
 		log.Fatal("Listener error:", err)
 	}
