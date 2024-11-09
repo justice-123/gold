9dd376316aa5d5816c8b1988e78397209f445f6c
Kanghyeon5468 <142624826+Kanghyeon5468@users.noreply.github.com>
2024-11-09 01:36:22 +0000

    Update server.go

--- server/server.go
+++ server/server.go
@@ -315,7 +315,7 @@ func calculateNextWorld(currentWorld [][]uint8, size, workerNum int) [][]uint8 {
 		splitSegments[i] = make(chan [][]uint8)
 	}
 
-	serverAddress := "18.171.135.180"
+	serverAddress := "172.31.24.54"
 	workerPorts := [8]string{":8040", ":8050", ":8060", ":8070", ":8080", ":8090", ":9000", ":9010"}
 	workers := make([]*rpc.Client, workerNum)
 
